548cdba813ed228c3dd4d2e3ef947608a607ecfc
mymyname <1264673507@qq.com>
2023-10-16 13:21:25 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+  <lunzi />
+</template>
+
+<script setup>
+import lunzi from './components/lunzi.vue'
+
+export default{
+  name: 'App',
+  components: {
+    lunzi
+  }
+}
+
+</script>
