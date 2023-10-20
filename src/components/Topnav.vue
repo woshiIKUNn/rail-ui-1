e401b932de377555e99777db18adcc5b05e7cb92
mymyname <1264673507@qq.com>
2023-10-20 16:04:59 +0800

    增加切换按钮

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="topnav">
-      <div class="logo" @click="toggleMenu">LOGO</div>
+      <div class="logo" >LOGO</div>
       <ul class="menu">
         <li><router-link to="/">菜单1</router-link></li>
         <li><router-link to="/Doc">菜单2</router-link></li>
       </ul>
+      <img src="../assets/menu.svg" class="toggleAside" @click="toggleMenu">
     </div>
 </template>
 
 <script lang="ts">
 import { inject, Ref } from 'vue';
+
 export default {
   setup() {
-    const menuVisible = inject<Ref<boolean>>('xxx')
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
     const toggleMenu = () =>{
       menuVisible!.value = !menuVisible?.value
     }
@@ -28,6 +30,8 @@ export default {
     color:white;
     position: relative;
     z-index: 1;
+    justify-content: center;
+    align-items: center;
 > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -39,6 +43,20 @@ export default {
   > li {
     margin: 0 1em;
   }
+ }
+ > .toggleAside{
+  display: none;
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+ }
+ @media (max-width: 500px) {
+  > .menu { display: none; }
+  > .logo { margin: 0 auto; }
+  > .toggleAside{ display: inline-block;  }
  }
 }
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -32,7 +32,7 @@ import Topnav from '../components/Topnav.vue';
 export default {
     components: { Topnav },
     setup() {
-    const menuVisible = inject<Ref<boolean>>('xxx')
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
       return {menuVisible}
   }
 }
@@ -43,10 +43,6 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
   > h2 {
     margin-bottom: 4px;
   }
@@ -54,6 +50,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
