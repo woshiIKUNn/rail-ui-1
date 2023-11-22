329e989ffb5ce101dd401ebbb35b86c8fb79ce47
mymyname <1264673507@qq.com>
2023-11-22 23:01:19 +0800

    增加input

--- src/components/DocUI/Input1.demo.vue
+++ src/components/DocUI/Input1.demo.vue
@@ -1,5 +1,28 @@
-<template>
-    <div>
-        hi
-    </div>
-</template>
+<demo>
+    基础用法-双向绑定
+    </demo>
+    
+    <template>
+      <Input placeholder="请输入内容" v-model:value="text"/>
+      <span class="picked">{{text}}</span>
+    </template>
+    
+    <script lang="ts">
+    import {ref} from 'vue';
+    import Input from '../../lib/Input.vue';
+    
+    export default {
+      name: 'InputDemo1',
+      components: {Input},
+      setup(_props) {
+        const text = ref('');
+        return {text};
+      }
+    };
+    </script>
+    
+    <style lang="scss" scoped>
+    .picked{
+      padding: 10px 15px;
+    }
+    </style>
--- src/components/DocUI/InputDemo.vue
+++ src/components/DocUI/InputDemo.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        hi
-    </div>
+    <doc-title>Input 输入框示例</doc-title>
+    <Demo :component="Input1Demo" />
+    <Demo :component="Input2Demo" />
+    <Demo :component="Input3Demo" />
+    <Attr :columns="columns" :data="data"></Attr>
 </template>
+<script lang="ts">
+import Input1Demo from '../../components/DocUI/Input1.demo.vue';
+import Input2Demo from '../../components/DocUI/Input2.demo.vue';
+import Input3Demo from '../../components/DocUI/Input3.demo.vue';
+import Demo from '../Demo.vue'
+import Attr from '../Attr.vue'
+import { columns } from "../../lib/data";
+import { ref } from "vue";
+import DocTitle from '../DocTitle.vue';
+
+export default {
+  name: 'InputDoc',
+  components: {
+    Demo,
+    DocTitle, 
+    Attr
+   },
+  setup() {
+    const data = ref([
+      {
+        params: 'placeholder',
+        desc: '占位符',
+        type: 'string',
+        select: '任意字符串',
+        default: '空',
+      },
+      {
+        params: 'name',
+        desc: 'name属性',
+        type: 'string',
+        select: '任意字符串',
+        default: '空',
+      },
+      {
+        params: 'type',
+        desc: '文本框类型',
+        type: 'string',
+        select: '支持所有原生input的类型',
+        default: 'text',
+      },
+      {
+        params: 'disabled',
+        desc: '禁用',
+        type: 'boolean',
+        select: 'false/true',
+        default: 'false',
+      },
+    ]);
+    return {Input1Demo,Input2Demo,Input3Demo, data, columns};
+  }
+};
+
+</script>
